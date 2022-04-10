919ff4965efc0cab36e8fb776e6566616c19671f
Mark <markghabbour62@gmail.com>
2022-04-10 20:37:00 +0200

    adding unit tests

--- technical-test-pro-backend/src/main/java/com/maiia/pro/dto/AppointmentMapper.java
+++ technical-test-pro-backend/src/main/java/com/maiia/pro/dto/AppointmentMapper.java
@@ -1,6 +1,5 @@
 package com.maiia.pro.dto;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.maiia.pro.entity.Appointment;
@@ -8,18 +7,13 @@ import com.maiia.pro.service.ProAvailabilityService;
 
 @Component
 public class AppointmentMapper {
-	
-	@Autowired
-	ProAvailabilityService proAvailabilityService;
 
-	public Appointment toAppointment (AppointmentCreationDTO appointmentCreationDTO) {
-        return Appointment.builder()
-        		.patientId(appointmentCreationDTO.getPatientId())
-        		.practitionerId(appointmentCreationDTO.getPractitionerId())
-        		.startDate(appointmentCreationDTO.getStartDate())
-        		.endDate(appointmentCreationDTO.getStartDate().
-        				plusMinutes(proAvailabilityService.getAppointmentDurationInMinutes()))
-        		.build();
-    }
-	
+	public Appointment toAppointment(AppointmentCreationDTO appointmentCreationDTO) {
+		return Appointment.builder().patientId(appointmentCreationDTO.getPatientId())
+				.practitionerId(appointmentCreationDTO.getPractitionerId())
+				.startDate(appointmentCreationDTO.getStartDate()).endDate(appointmentCreationDTO.getStartDate()
+						.plusMinutes(ProAvailabilityService.getAppointmentDurationInMinutes()))
+				.build();
+	}
+
 }
