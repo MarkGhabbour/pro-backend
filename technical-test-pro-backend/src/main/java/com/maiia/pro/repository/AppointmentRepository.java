2608a94b2963dd14cfb168b65a1f587d1f75b9fa
mfareed@ejada.com <mfareed@ejada.com>
2022-04-10 13:17:59 +0200

    fix if appointments do not all start at multiples of 15 mins

--- technical-test-pro-backend/src/main/java/com/maiia/pro/repository/AppointmentRepository.java
+++ technical-test-pro-backend/src/main/java/com/maiia/pro/repository/AppointmentRepository.java
@@ -1,13 +1,11 @@
 package com.maiia.pro.repository;
 
-import com.maiia.pro.entity.Appointment;
-import com.maiia.pro.entity.Patient;
-import com.maiia.pro.entity.TimeSlot;
+import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.maiia.pro.entity.Appointment;
 
 @Repository
 public interface AppointmentRepository extends CrudRepository<Appointment, Integer> {
--- technical-test-pro-backend/src/main/java/com/maiia/pro/service/ProAvailabilityService.java
+++ technical-test-pro-backend/src/main/java/com/maiia/pro/service/ProAvailabilityService.java
@@ -34,7 +34,8 @@ public class ProAvailabilityService {
 	public List<Availability> generateAvailabilities(Integer practitionerId) {
 		// TODO : implement this
 		List<TimeSlot> timeSlotList = timeSlotRepository.findByPractitionerIdOrderByStartDateAsc(practitionerId);
-		List<Appointment> bookedAppointmentList = appointmentRepository.findByPractitionerIdOrderByStartDateAsc(practitionerId);
+		List<Appointment> bookedAppointmentList = appointmentRepository
+				.findByPractitionerIdOrderByStartDateAsc(practitionerId);
 
 		List<Availability> freeAvailabilities = new ArrayList<Availability>();
 
@@ -43,35 +44,39 @@ public class ProAvailabilityService {
 		for (int i = 0; i < timeSlotList.size(); i++) {
 			TimeSlot timeslot = timeSlotList.get(i);
 
-			int j = 0;
-
 			LocalDateTime startDateTime = timeslot.getStartDate();
 			LocalDateTime endDateTime = timeslot.getEndDate();
 
 			while (endDateTime.isAfter(startDateTime)) {
-				// generate possible availabilities time from timeslot
-				Availability freeAvailability = Availability.builder()
-						.startDate(startDateTime)
+				// generate possible availabilities from timeSlot
+				Availability freeAvailability = Availability.builder().startDate(startDateTime)
 						.endDate(startDateTime.plusMinutes(appointmentDurationInMinutes)).practitionerId(practitionerId)
 						.build();
 
 				if (bookedAppointmentPointer < bookedAppointmentList.size()) {
 					Appointment bookedAppointment = bookedAppointmentList.get(bookedAppointmentPointer);
-					if (bookedAppointment.getStartDate().equals(freeAvailability.getStartDate())
-							&& bookedAppointment.getEndDate().equals(freeAvailability.getEndDate()))
+
+					boolean isAfterStart = bookedAppointment.getStartDate().equals(freeAvailability.getStartDate())
+							|| bookedAppointment.getStartDate().isAfter(freeAvailability.getStartDate());
+
+					boolean isBeforeEnd = bookedAppointment.getStartDate().isBefore(freeAvailability.getEndDate());
+
+					//check if appointment startTime is between availability start and end
+					if (isAfterStart && isBeforeEnd) {
 						bookedAppointmentPointer++;
-					else {
+						startDateTime = bookedAppointment.getEndDate();
+					} else {
 						freeAvailabilities.add(freeAvailability);
+						startDateTime = startDateTime.plusMinutes(appointmentDurationInMinutes);
 					}
 				} else {
 					freeAvailabilities.add(freeAvailability);
+					startDateTime = startDateTime.plusMinutes(appointmentDurationInMinutes);
 				}
-				j++;
-				startDateTime=startDateTime.plusMinutes(appointmentDurationInMinutes);
+
 			}
 		}
 
-		// return freeAvailabilities;
 		return freeAvailabilities;
 
 	}
